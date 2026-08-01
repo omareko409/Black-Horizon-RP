1bea138530ed1927b833ae01f76e18080f027eda
omareko407 <omareko407@gmail.com>
2026-08-02 02:21:07 +0300

    Fix import path for authOptions

--- src/app/api/admin/accept-application/route.js
+++ src/app/api/admin/accept-application/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../../auth/[...nextauth]/route"; // Assuming authOptions is exported
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 const ADMIN_ROLES = [
   "1532005442602008677",
